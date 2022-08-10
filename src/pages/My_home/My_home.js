b4202f4e5682d44a0d8c8c207a3e14ca9096c9a5
nelumbo <remi.chambiet@laposte.net>
2022-08-10 15:38:35 +0200

    premiere version fonctionnel du usefecth

--- src/pages/My_home/My_home.js
+++ src/pages/My_home/My_home.js
@@ -1,38 +1,24 @@
 import React from "react";
 import Indicator from "../../component/indicator/indicator";
-import {USER_MAIN_DATA,USER_ACTIVITY,USER_AVERAGE_SESSIONS,USER_PERFORMANCE} from "../../data";
 import Barweight from "../../component/graph/histogram/histogram";
 import Timeline from "../../component/graph/curve/curve";
 import RadarProfile from "../../component/graph/radar/radar";
 import Circle from "../../component/graph/circle_dia/circle";
 import useFetch from "../../tools/useFetch";
+import GetData from "../../tools/Data_services";
 
 
 const id=12;
 
-
-const data_act=USER_ACTIVITY.find(function (data_act) {
-  if(data_act.userId===id){
-    return data_act;
-  }
-});
-const data_time=USER_AVERAGE_SESSIONS.find(function (data_time) {
-  if(data_time.userId===id){
-    return data_time;
-  }
-});    
-const data_perf=USER_PERFORMANCE.find(function (data_perf) {
-  if(data_perf.userId===id){
-    return data_perf;
-  }
-});    
-
-
-
 function My_home() {
 
   const main_data =useFetch("http://localhost:3000/user/12");
-if(main_data){
+  const data_act =useFetch("http://localhost:3000/user/12/activity");
+  const data_time =useFetch("http://localhost:3000/user/12/average-sessions");
+  const data_perf =useFetch("http://localhost:3000/user/12/performance");
+
+
+if(main_data && data_act && data_perf && data_time){
   const calories=main_data.data.keyData.calorieCount+"kcal";
   const proteines=main_data.data.keyData.proteinCount+"gr";
   const glucides=main_data.data.keyData.carbohydrateCount+"gr";
@@ -50,9 +36,9 @@ return (
         <p className="copright">Copiryght, SportSee 2020</p>
       </div>  
       <header className="header_profile"><div className="wrapper_welcome"><h1>Bonjour </h1><h1 className="firstname"> {main_data.data.userInfos.firstName}</h1></div><p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p></header>
-      <Barweight databar={data_act.sessions}></Barweight>
-      <Timeline dataline={data_time.sessions}></Timeline>
-      <RadarProfile dataradar={data_perf.data}></RadarProfile>
+      <Barweight databar={data_act.data.sessions}></Barweight>
+      <Timeline dataline={data_time.data.sessions}></Timeline>
+      <RadarProfile dataradar={data_perf.data.data}></RadarProfile>
       <Circle datacircle={main_data.data}></Circle>
       <aside className="wrapper_indi">
       <Indicator img="/calories-icon.png" name="calories" value={calories}></Indicator>
--- src/tools/Data_services.js
+++ src/tools/Data_services.js
@@ -1,7 +1,26 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
+function GetData(id) {
+
+    let data_conv=null;
+    
+    this.main_data = useFetch(`http://localhost:3000/user/${id}`);;
+    this.data_act = useFetch(`http://localhost:3000/user/${id}/activity`);
+    this.data_time = useFetch(`http://localhost:3000/user/${id}/average-sessions`);
+    this.data_perf = useFetch(`http://localhost:3000/user/${id}/performance`);
+    this.checkdata = function() {
+        if (this.main_data && this.data_act && this.data_perf && this.data_time) 
+        {
+                        return data_conv=[
+                        this.main_data.data,
+                        this.data_act.data,
+                        this.data_time.data,
+                        this.data_perf.data
+                    ] 
+                }
+    }
+}   
+ export default GetData;
 
 
-
-// const main_data =useFetch("http://localhost:3000/user/12");
